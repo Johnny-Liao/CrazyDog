486b4de29e318582a98f25db265df9156be97aa8
Daniel Chiu <1360874697@qq.com>
2015-08-24 21:22:48 +0800

    解决了在测试时由于HibernateTemplate的默认设置flushmode为manual，在配置文件中更改为写入模式，以允许写入

--- src/main/java/org/crazydog/daoI/impl/ResumedaoImpl.java
+++ src/main/java/org/crazydog/daoI/impl/ResumedaoImpl.java
@@ -1,51 +1,40 @@
 package org.crazydog.daoI.impl;
 
-import java.util.List;
-
 import org.crazydog.daoI.ResumedaoI;
-import org.crazydog.domain.HireEntity;
 import org.crazydog.domain.ResumeEntity;
-import org.crazydog.utils.SpringHelper;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Component("resumedaoImpl")
-public class ResumedaoImpl implements ResumedaoI{
-	
-	@Autowired(required=true)
-	@Qualifier(value="hibernateTemplate")
-	private HibernateTemplate hibernateTemplate;
+public class ResumedaoImpl implements ResumedaoI {
 
-	@Override
-	public void addResume(ResumeEntity entity) {
-		hibernateTemplate.save(entity);
-	}
-	@Transactional(readOnly=false)
-	@Override
-	public void modifyResume(ResumeEntity entity) {
-		hibernateTemplate.update(entity);
-	}
+    @Autowired(required = true)
+    @Qualifier(value = "hibernateTemplate")
+    private HibernateTemplate hibernateTemplate;
 
-	@Override
-	public void deleteResume(int id) {
-		hibernateTemplate.delete(this.getResume(id));
-	}
+    public void addResume(ResumeEntity entity) {
+        hibernateTemplate.save(entity);
+    }
 
-	@Override
-	public ResumeEntity getResume(int id) {
-		return hibernateTemplate.get(ResumeEntity.class, id);
-	}
+    @Transactional(readOnly = false)
+    public void modifyResume(ResumeEntity entity) {
+        hibernateTemplate.update(entity);
+    }
 
-	@Override
-	public List<ResumeEntity> getAllResumes() {
-		return hibernateTemplate.loadAll(ResumeEntity.class);
-	}
+    public void deleteResume(int id) {
+        hibernateTemplate.delete(this.getResume(id));
+    }
+
+    public ResumeEntity getResume(int id) {
+        return hibernateTemplate.get(ResumeEntity.class, id);
+    }
+
+    public List<ResumeEntity> getAllResumes() {
+        return hibernateTemplate.loadAll(ResumeEntity.class);
+    }
 }
