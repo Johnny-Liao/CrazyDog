b00bf2e5a2e448094b707ee08b71f2ee3cade5a1
Johnny-Liao <1020157553@qq.com>
2015-08-27 19:42:57 +0800

    some change

--- src/test/java/org/crazydog/serviceI/impl/ResumeServiceTest.java
+++ src/test/java/org/crazydog/serviceI/impl/ResumeServiceTest.java
@@ -17,56 +17,55 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
 public class ResumeServiceTest {
-	ApplicationContext applicationContext;
-	ResumeServiceImpl resumeServiceImpl;
-	Basedao basedao;
+    ApplicationContext applicationContext;
+    ResumeServiceImpl resumeServiceImpl;
+    Basedao basedao;
 
-	@Before
-	public void setUp() throws Exception {
-		applicationContext = new ClassPathXmlApplicationContext(
-				"test-application-config.xml");
+    @Before
+    public void setUp() throws Exception {
+        applicationContext = new ClassPathXmlApplicationContext(
+                "test-application-config.xml");
 
-	}
+    }
 
-	// @Test
-	// public void test(){
-	// resumeServiceImpl = (ResumeServiceImpl)
-	// applicationContext.getBean("resumeServiceImpl");
-	// System.out.println(resumeServiceImpl);
-	// // 测试取消录取
-	// resumeServiceImpl.cancelHire(1,"廖仕豪");
-	// }
-	// @Test
-	// public void test(){
-	// // 测试通过resumeid拿取录取信息
-	// resumeServiceImpl = (ResumeServiceImpl)
-	// applicationContext.getBean("resumeServiceImpl");
-	// System.out.println(resumeServiceImpl);
-	// HireEntity h = resumeServiceImpl.getEntitybyreumeid(3);
-	// System.out.println(h);
-	// }
-//	@Test
-//	public void test() {
-//		  //简历的复杂查询 
-//		resumeServiceImpl = (ResumeServiceImpl) applicationContext
-//				.getBean("resumeServiceImpl");
-//		SearchModel sreach = new SearchModel(null, "博士", "二审");
-//		String sql = sreach.advanceSearch();
-//		System.out.println(sql);
-//	    List<Object> list=resumeServiceImpl.queryEntity(sql);
-//		System.out.println((Object)list.get(0));
-////		for(int i=0;i<list.size();i++){
-////			Object[] b = (Object[]) list.get(i);
-////			for(int j=0;j<b.length;j++){
-////				System.out.println(b[j]);
-////			}
-////		}
+    // @Test
+    // public void test(){
+    // resumeServiceImpl = (ResumeServiceImpl)
+    // applicationContext.getBean("resumeServiceImpl");
+    // System.out.println(resumeServiceImpl);
+    // // 测试取消录取
+    // resumeServiceImpl.cancelHire(1,"廖仕豪");
+    // }
+    // @Test
+    // public void test(){
+    // // 测试通过resumeid拿取录取信息
+    // resumeServiceImpl = (ResumeServiceImpl)
+    // applicationContext.getBean("resumeServiceImpl");
+    // System.out.println(resumeServiceImpl);
+    // HireEntity h = resumeServiceImpl.getEntitybyreumeid(3);
+    // System.out.println(h);
+    // }
+    @Test
+    public void test() {
+        //简历的复杂查询
+        resumeServiceImpl = (ResumeServiceImpl) applicationContext.getBean("resumeServiceImpl");
+        SearchModel sreach = new SearchModel(null, "博士", "二审");
+        String sql = sreach.advanceSearch();
+        System.out.println(sql);
+        List<Object> list = resumeServiceImpl.queryEntity(sql);
+        System.out.println((Object) list.get(0));
+        for (int i = 0; i < list.size(); i++) {
+            Object[] b = (Object[]) list.get(i);
+            for (int j = 0; j < b.length; j++) {
+                System.out.println(b[j]);
+            }
+        }
 //		Object[] b = (Object[]) list.get(0);
 //		HireEntity h = (HireEntity) b[1];
 //		System.out.println(h.getOperator());
-//	}
-	@Test
-	public void test() {
+    }
+/*	@Test
+    public void test() {
 		  //测试获取所有的教育经历记录
 		resumeServiceImpl = (ResumeServiceImpl) applicationContext
 				.getBean("resumeServiceImpl");
@@ -77,7 +76,7 @@ public class ResumeServiceTest {
 			ResumeEduEntity a = it.next();
 			System.out.println(a.getSchool());
 		}
-	}
+	}*/
 
 
 }
