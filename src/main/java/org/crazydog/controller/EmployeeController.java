5130db00d2741148e5eeeacdd30ba004ae950aaf
Johnny-Liao <1020157553@qq.com>
2015-08-27 13:57:25 +0800

    Add EmployeeController.java

---
+++ src/main/java/org/crazydog/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package org.crazydog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * Created by johnny on 15-8-27.
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+
+    // eg : .../employee?pages=1 will get the information about the first page employee
+    @RequestMapping(method = RequestMethod.GET, params = "pages")
+    public ModelAndView getEmployeeByPages(String page) {
+        return null;
+    }
+
+}
