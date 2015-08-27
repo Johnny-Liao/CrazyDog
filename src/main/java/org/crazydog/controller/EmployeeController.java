2dfb8af971964c06f70aaa223cfcf565c453241c
Johnny-Liao <1020157553@qq.com>
2015-08-27 14:14:41 +0800

    a changed.

--- src/main/java/org/crazydog/controller/EmployeeController.java
+++ src/main/java/org/crazydog/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -18,7 +19,7 @@ public class EmployeeController {
 
     // eg : .../employee?pages=1 will get the information about the first page employee
     @RequestMapping(method = RequestMethod.GET, params = "pages")
-    public ModelAndView getEmployeeByPages(String page) {
+    public ModelAndView getEmployeeByPages(@RequestParam("pages") String page, Model model) {
         return null;
     }
 
